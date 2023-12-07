dfbbc814acd12c7d13d651ca187a87440f493001
Hudson-Matias <hudson.um@gmail.com>
2023-12-07 09:46:09 -0300

    Comentários e DocStrings

--- aula1.py
+++ aula1.py
@@ -1,1 +1,20 @@
 print('Explícito', 'é', 'melhor " do que implícito')
+
+
+"""
+DocString -> Aspas duplas
+E escrever o que 
+eu quiser
+"""
+
+'''
+DocString -> Aspas simples
+Usar para escrever suas notas
+'''
+
+# Comentário para uma única linha
+
+# permite escrever um comentário
+print(123) # na frente
+# abaixo
+print(456)
