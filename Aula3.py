558c2613cfe0da6305919fcab726efce259fa01a
Hudson-Matias <hudson.um@gmail.com>
2023-12-07 11:21:17 -0300

    Tipo str (string) - Introdução aos tipos de dados

---
+++ Aula3.py
@@ -0,0 +1,22 @@
+"""
+Python = Linguagem de Programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+#print(1234)
+
+# Aspas simples
+#print('Hudson Matias')
+#print('Hudson "Matias"')
+
+# Aspas duplas
+print("Hudson Matias")
+print("Hudson 'Matias'")
+
+# Escape
+#print("Hudson \"Matias\"")
+
+# r
+#print(r"Hudson \"Matias\"")
+
