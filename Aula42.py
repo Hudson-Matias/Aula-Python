871654415e53de721a7f79c8357f8b30ab3aa514
Hudson-Matias <hudson.um@gmail.com>
2023-12-07 16:01:45 -0300

    Atualizo depois

---
+++ Aula42.py
@@ -0,0 +1,12 @@
+frase = 'O Python é uma linguagem de programação '\
+        'multiparadigma. '\
+        'Python foi criado por Guido van Rossun'
+
+i = 0
+while i < len(frase):
+
+    letra_atual = frase[i]
+
+    print(letra_atual)
+    i += 1
+
